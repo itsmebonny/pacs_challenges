1f9d1f657f3bb0bc104d75119e87e52502b86d60
Andrea <andrea.bonifacio.000@gmail.com>
2023-03-15 17:01:19 +0100

    almost done

--- 01_challenge/include/cauchy_solver.hpp
+++ 01_challenge/include/cauchy_solver.hpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include "gnuplot-iostream.hpp"
+#include <set>
 
 class cauchy_solver
 {
@@ -20,11 +21,10 @@ private:
     double m_N;
     std::array<std::vector<double>, 2> m_sol;
 
-    double crank_nicolson(std::function<double (const double &, const double &)> fun, double x0, double t0, double h){
+    double step(std::function<double (const double &, const double &)> fun, double x0, double t0, double h, double theta){
         double fn = fun(t0, x0);
-        std::cout << fn << std::endl;
         auto fn1 = [&](double x){
-            return x - h * 0.5 * (fun(t0+h, x) + fn) - x0;
+            return x - h * theta * fun(t0+h, x) - h * (1 - theta) *  fn - x0;
         };
         double x1 = x0 + h * fn;
         if (x0 == x1){ //because i'm stupid
@@ -32,7 +32,6 @@ private:
         }
         std::tuple<double, bool> zero = apsc::secant <std::function<double (const double &)>> (fn1, x0, x1);
 
-        std::cout << "zero at : " << std::get<1>(zero) << std::endl;
         return std::get<0>(zero);
     };
 
@@ -40,44 +39,86 @@ public:
     cauchy_solver(std::function<double (const double &, const double &)> fun, double  x0, double t0, double T, double N)
      : m_fun(fun), m_x0(x0), m_t0(t0), m_T(T), m_N(N) {};
     
-    std::array<std::vector<double>, 2> solve(void){
+    std::array<std::vector<double>, 2> solve(double theta = 0.5){
+        if (theta > 1 && theta < 0){
+            std::cerr << "theta must be inside [0,1]!" << std::endl;
+            std::cout << "Using theta = 0.5" << std::endl;;
+            theta = 0.5;
+        }
+        std::cout << "theta = " << theta << std::endl;
         auto h = (m_T-m_t0)/m_N;
         m_sol[0].push_back(m_t0);
         m_sol[1].push_back(m_x0);
+        std::cout << 0 << " un: " << m_x0 << std::endl;
 
         for (size_t n = 1; n < m_N; n++)
         {
-            double un = crank_nicolson(m_fun, m_sol[1].back(), m_sol[0].back(), h);
-            std::cout << "un: " << un << std::endl;
+            double un = step(m_fun, m_sol[1].back(), m_sol[0].back(), h, theta);
+            std::cout <<n << " un: " << un << std::endl;
             m_sol[0].push_back(n*h);
             m_sol[1].push_back(un);
         }
+        
         return m_sol;
     }
-    void save_solution(std::string filename){
-        if(m_sol.empty()){
+    void print_solution(void){
+        if(m_sol[0].empty() || m_sol[1].empty()){
             std::cerr << "You need to run the solve method first!" << std::endl;
         }
-        std::ofstream solution(filename);
-        bool flag = true;
-        for (auto i : m_sol)
-        {
-            if (flag){
-                std::cout << "tn: ";
-                solution << "tn: ";
+        else{
+            bool flag = true;
+            for (auto i : m_sol){
+                if (flag){
+                    std::cout << "tn: ";
+                }
+                else{
+                    std::cout << "un: ";
+                }
+                for (auto j : i)
+                {
+                    std::cout << j << ", ";
+                    if (i.back() == j){
+                        std::cout << j;
+                    }
+                }
+                std::cout << std::endl;
+                flag = false;
             }
-            else{
-                std::cout << "un: ";
-                solution << "un: ";
+        }
+    }
+    void save_solution(std::string filename){
+        if(m_sol[0].empty() || m_sol[1].empty()){
+            std::cerr << "You need to run the solve method first!" << std::endl;
+        }
+        else{
+            std::ofstream solution(filename);
+            bool flag = true;
+            for (auto i : m_sol){
+                if (flag){
+                    solution << "tn: ";
+                }
+                else{
+                    solution << "un: ";
+                }
+                for (auto j : i)
+                {
+                    solution << j << ", ";
+                    if (i.back() == j){
+                        solution << j;
+                    }
+                }
+                solution << std::endl;
+                flag = false;
             }
-            for (auto j : i)
-            {
-                std::cout << j << ", ";
-                solution << j << ", ";
-            }
-            std::cout << std::endl;
-            solution << std::endl;
-            flag = false;
+        }
+    }
+    void plot(void){
+        if(!(m_sol[0].empty() || m_sol[1].empty())){
+        Gnuplot gp; 
+        gp << "plot" << gp.file1d(std::tie(m_sol[0], m_sol[1])) << "title 'uh'" << std::endl;
+        }
+        else{
+            std::cerr << "The solution is empty! Run the solve method before" << std::endl;
         }
     }
 };
--- 01_challenge/main.cpp
+++ 01_challenge/main.cpp
@@ -6,14 +6,25 @@
 double function (const double &t, const double &y){
     return -t*std::exp(-y);
 }
-int main(){
+int main(int argc, char* argv[]){
     
     std::function<double (const double &, const double &)> fun = function;
     double x0 = 0;
-    cauchy_solver test(fun, x0, 0, 1, 100);
-    auto sol = test.solve();
-    test.save_solution("test.txt");
-    
+    cauchy_solver test1(fun, x0, 0, 1, 100);
+    cauchy_solver test2(fun, x0, 0, 1, 100);
+    cauchy_solver test3(fun, x0, 0, 1, 100);
+    auto sol1 = test1.solve(1);
+    std::cout << sol1[0].size() << std::endl;
+    auto sol2 = test2.solve(0);
+    std::cout << sol2[0].size() << std::endl;
+    auto sol3 = test3.solve(0.5);
+    std::cout << sol3[0].size() << std::endl;
+    test1.save_solution("final.csv");
+    //test.print_solution();
     Gnuplot gp; 
-    gp << "plot" << gp.file1d(std::tie(sol[0], sol[1])) << "title 'uh'" << std::endl;
-}
+    gp << "plot" 
+    << gp.file1d(std::tie(sol1[0], sol1[1])) << "w lp pt 1 title 'bw eu',"
+    << gp.file1d(std::tie(sol2[0], sol2[1])) << "w lp pt 2 title 'fw eu',"
+    << gp.file1d(std::tie(sol3[0], sol3[1])) << "w lp pt 3 title 'cn'" 
+    << std::endl;
+    }
