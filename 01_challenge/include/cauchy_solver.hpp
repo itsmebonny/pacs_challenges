e69a31d89d246b937a3608939e7fac266c4ea426
Andrea <andrea.bonifacio.000@gmail.com>
2023-03-15 23:40:43 +0100

    could be the final v.

--- 01_challenge/include/cauchy_solver.hpp
+++ 01_challenge/include/cauchy_solver.hpp
@@ -19,9 +19,15 @@ private:
     double m_t0;
     double m_T;
     size_t m_N;
+    bool m_print = false;
+    bool m_plot = false;
+    bool m_save = true;
     std::array<std::vector<double>, 2> m_sol;
 
     double step(std::function<double (const double &, const double &)> fun, double x0, double t0, double h, double theta){
+        /*
+        the step of the numeric method, depends on the paramether theta
+        */
         double fn = fun(t0, x0);
         auto fn1 = [&](double x){
             return x - h * theta * fun(t0+h, x) - h * (1 - theta) *  fn - x0;
@@ -34,10 +40,51 @@ private:
 
         return std::get<0>(zero);
     };
-
+    void print_solution(void){
+        /*
+        a simple method to print the solution on the screen
+        */
+        if(m_sol[0].empty() || m_sol[1].empty()){
+            std::cerr << "You need to run the solve method first!" << std::endl;
+        }
+        else{
+            for (size_t i = 0; i < m_sol[0].size(); i++)
+            {
+                std::cout << m_sol[0][i] << "\t" << m_sol[1][i] << std::endl;
+            }
+        }
+    }
+    void save_solution(std::string filename = "solution.csv"){
+        /*
+        this one stores the solution in a file, if the name changes, must be changed also in plot.p, otherwise make plot won't work
+        */
+        if ((m_sol[0].empty() || m_sol[1].empty()) || (m_sol[1].size() != m_sol[0].size())){
+            std::cerr << "You need to run the solve method first!" << std::endl;
+        }
+        else{
+            std::ofstream solution(filename);
+            for (size_t i = 0; i < m_sol[0].size(); i++)
+            {
+                solution << m_sol[0][i] << "," << m_sol[1][i] << ";" << std::endl;
+            }
+        }
+    }
+    
+    void plot_solution(void){
+        /*
+        this method plots the solution, i used it to get familiar with gnuplot
+        */
+        if(!(m_sol[0].empty() || m_sol[1].empty())){
+        Gnuplot gp; 
+        gp << "plot" << gp.file1d(std::tie(m_sol[0], m_sol[1])) << "title 'uh'" << std::endl;
+        }
+        else{
+            std::cerr << "The solution is empty! Run the solve method before" << std::endl;
+        }
+    }
 public:
-    cauchy_solver(std::function<double (const double &, const double &)> fun, double  x0, double t0, double T, size_t N)
-     : m_fun(fun), m_x0(x0), m_t0(t0), m_T(T), m_N(N) {};
+    cauchy_solver(std::function<double (const double &, const double &)> fun, double  x0, double t0, double T, size_t N, bool print = false, bool plot = false, bool save = true)
+     : m_fun(fun), m_x0(x0), m_t0(t0), m_T(T), m_N(N), m_print(print), m_save(save), m_plot(plot) {};
     
     std::array<std::vector<double>, 2> solve(double theta = 0.5){
         if (theta > 1 && theta < 0){
@@ -56,56 +103,18 @@ public:
             m_sol[0].push_back(n*h);
             m_sol[1].push_back(un);
         }
-        
+        if(m_save){
+            save_solution();
+        }
+        if(m_plot){
+            plot_solution();
+        }
+        if(m_print){
+            print_solution();
+        }
         return m_sol;
     }
-    void print_solution(void){
-        if(m_sol[0].empty() || m_sol[1].empty()){
-            std::cerr << "You need to run the solve method first!" << std::endl;
-        }
-        else{
-            bool flag = true;
-            for (auto i : m_sol){
-                if (flag){
-                    std::cout << "tn: ";
-                }
-                else{
-                    std::cout << "un: ";
-                }
-                for (auto j : i)
-                {
-                    std::cout << j << ", ";
-                    if (i.back() == j){
-                        std::cout << j;
-                    }
-                }
-                std::cout << std::endl;
-                flag = false;
-            }
-        }
-    }
-    void save_solution(std::string filename = "solution.csv"){
-        if ((m_sol[0].empty() || m_sol[1].empty()) || (m_sol[1].size() != m_sol[0].size())){
-            std::cerr << "You need to run the solve method first!" << std::endl;
-        }
-        else{
-            std::ofstream solution(filename);
-            for (size_t i = 0; i < m_sol[0].size(); i++)
-            {
-                solution << m_sol[0][i] << "," << m_sol[1][i] << ";" << std::endl;
-            }
-        }
-    }
     
-    void plot(void){
-        if(!(m_sol[0].empty() || m_sol[1].empty())){
-        Gnuplot gp; 
-        gp << "plot" << gp.file1d(std::tie(m_sol[0], m_sol[1])) << "title 'uh'" << std::endl;
-        }
-        else{
-            std::cerr << "The solution is empty! Run the solve method before" << std::endl;
-        }
-    }
 };
 
 #endif //CAUCHY_SOLVER_HH
--- 01_challenge/main.cpp
+++ 01_challenge/main.cpp
@@ -2,18 +2,19 @@
 #include "GetPot"
 
 double function (const double &t, const double &y){
+    /*
+    the function of the problem
+    */
     return -t*std::exp(-y);
 }
-int main(int argc, char* argv[]){
-    
-
-    GetPot ifl("parameters.pot");
+int main(){
+    GetPot ifl("parameters.pot"); //parameters stored here, head to the file to see what those variables are
     size_t N = ifl("N", 150);
     std::function<double (const double &, const double &)> fun = function;
     double x0 = ifl("x0", 0);
     double theta = ifl("theta", 0.5);
-    cauchy_solver test(fun, x0, 0, 1, N);
-    auto sol1 = test.solve(theta);
-    test.save_solution();
-    test.print_solution();
+    bool print = ifl("print_solution", false), plot = ifl("plot_solution", false), save = ifl("save_solution", true);
+    cauchy_solver obj(fun, x0, 0, 1, N, print, plot, save);
+    obj.solve(theta);
+    return 0;
     }
